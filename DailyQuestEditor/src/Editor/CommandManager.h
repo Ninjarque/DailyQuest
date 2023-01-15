1bcd53b3eb672213136906e58fff78ae28c6d4ba
Ninjarque <ninjarque@gmail.com>
2023-01-15 21:06:08 +0100

    Début de création de CommandManager pour la pattern Command

--- DailyQuestEditor/src/Editor/CommandManager.h
+++ DailyQuestEditor/src/Editor/CommandManager.h
@@ -13,6 +13,36 @@ class CommandQueue
 {
 public:
 
+	void DisposeNext()
+	{
+		if (_next != nullptr) _next->DisposeNext();
+		if (_command != nullptr) delete _command;
+
+		_next = nullptr;
+		_command = nullptr;
+	}
+
+	void Push(ICommand* command)
+	{
+		if (_command != nullptr) _next->Push(command);
+		else
+		{
+			_command = command;
+			_next = new CommandQueue();
+			_next->_previous = this;
+		}
+	}
+	void SetCommand(ICommand* command)
+	{
+		_command = command;
+		if (_next != nullptr)
+		{
+			_next->DisposeNext();
+			delete _next;
+			_next = nullptr;
+		}
+	}
+
 private:
 	CommandQueue* _previous = nullptr;
 	CommandQueue* _next = nullptr;
@@ -23,8 +53,13 @@ private:
 class CommandManager
 {
 public:
+	bool CanDo();
+	bool CanUndo();
 
+	void Do();
+	void Undo();
 private:
-	CommandQueue _commands;
+	CommandQueue* _head;
+	CommandQueue* _commands;
 };
 
