ffade883ddc00ae505fe5d2d23e1cc12455a9254
Ninjarque <ninjarque@gmail.com>
2023-01-25 12:09:18 +0100

    Déplacement du code de Story.cpp dans son .h

--- DailyQuestCore/src/Story/Story.h
+++ DailyQuestCore/src/Story/Story.h
@@ -1,37 +1,63 @@
 #pragma once
 
+#include <vector>
+#include <tuple>
+
 #include "entt/entt.hpp"
 
 #include "Entity.h"
-#include "StoryManager.h"
+//#include "StoryManager.h"
 
 #include "Quest/Quest.h"
 
-class Entity;
-class StoryManager;
+//class Entity;
+//class StoryManager;
 
 class Story
 {
 public:
-	Story();
-	~Story();
-	static std::unique_ptr<Story> Create();
+	~Story()
+	{
 
-	Entity CreateEntity();
+	}
+
+	Entity CreateEntity()
+	{
+		return Entity(_registry);
+	}
 	template <typename T, typename... Args>
-	Entity CreateEntity(Args&&... args);
+	Entity CreateEntity(Args&&... args)
+	{
+		Entity entity = Entity(_registry);
+		entity.Add<T, Args>(args);
+		return entity;
+	}
 
 
-	std::unique_ptr<Quest> CreateQuest();
-
-
-	void Update(float deltaTime);
-	void Draw();
+	std::unique_ptr<Quest> CreateQuest()
+	{
+		return std::unique_ptr<Quest>();
+	}
 
 private:
+	Story() { }
+	void Update(float deltaTime) { }
+	void Draw() { }
 
-	entt::registry _registry;
+	template<typename... Component>
+	std::tuple<std::vector<Component>...> GetAll()
+	{
+		std::tuple<std::vector<Component>...> components;
+		auto view = _registry->view<Component...>();
+		for (auto entity : view) {
+			((std::get<std::vector<Component>>(components).push_back(_registry->get<Component>(entity)), ...));
+		}
+		return components;
+	}
+	
+	std::shared_ptr<entt::registry> _registry;
 	std::weak_ptr<Story> _self;
 
 	friend class Entity;
+	friend class StoryManager;
 };
