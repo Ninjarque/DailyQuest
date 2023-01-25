2a36bdf7458c3739b0c256ba4af2042a74347942
Ninjarque <ninjarque@gmail.com>
2023-01-25 12:09:09 +0100

    Déplacement du code de Entity.cpp dans son .h

--- DailyQuestCore/src/Story/Entity.h
+++ DailyQuestCore/src/Story/Entity.h
@@ -2,10 +2,9 @@
 
 #include <entt/entt.hpp>
 
-#include "Story.h"
 #include "Error.h"
 
-class Story;
+#include "Quest/Name.h"
 
 class Entity
 {
@@ -13,23 +12,67 @@ public:
 	template <typename T>
 	bool Has();
 
+	template <typename T>
+	T& Get();
+
 	template <typename T, typename... Args>
 	T& Add(Args&&... args);
 
 	template <typename T>
-	T& Get();
-
-	template <typename T>
 	bool Remove();
 
-	Entity();
+	Entity()
+	{
+	}
+	~Entity()
+	{
+		_registry->destroy(_handle);
+	}
 private:
-	Entity(entt::entity handle, Story* story);
-	Entity(const Entity& entity) = default;
-	~Entity();
+	Entity(std::shared_ptr<entt::registry> registry)
+	{
+		_handle = registry->create();
+		_registry = registry.get();
+		Add<std::shared_ptr<Name>>();
+	}
 
 	entt::entity _handle{0};
-	Story* _story = nullptr;
+	entt::registry* _registry = nullptr;
 
 	friend class Story;
 };
+
+template<typename T>
+bool Entity::Has()
+{
+	return _registry->all_of<T>(_handle);
+}
+
+template<typename T>
+T& Entity::Get()
+{
+	if (!Has<T>()) Error::fatalError("Could not get component because it hasn't been added yet!");
+
+	return _registry->get<T>(_handle);
+}
+
+
+template<typename T, typename ...Args>
+T& Entity::Add(Args&&... args)
+{
+	//if (Has<T>()) Error::fatalError("Could not add component because it already has been added!");
+
+	return _registry->get_or_emplace<T>(_handle, std::forward<Args>(args)...);
+}
+
+template<typename T>
+bool Entity::Remove()
+{
+	if (!Has<T>())
+	{
+		Error::fatalError("Could not remove component because it hasn't been added yet!");
+		return false;
+	}
+	_registry->remove<T>(_handle);
+	return true;
+}
