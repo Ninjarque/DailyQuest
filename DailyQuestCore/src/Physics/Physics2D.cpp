c694fd04be48b4d2a12ab1db88d478c358518c59
Ninjarque <ninjarque@gmail.com>
2023-01-25 12:10:29 +0100

    Utilisation de StoryManager pour récupérer les composants de la scène

--- DailyQuestCore/src/Physics/Physics2D.cpp
+++ DailyQuestCore/src/Physics/Physics2D.cpp
@@ -1,7 +1,8 @@
 #include "Physics2D.h"
 
 std::unique_ptr<b2World> Physics2D::_world;
-std::unordered_map<std::shared_ptr<Name>, b2Body*> Physics2D::_bodies;
+std::unordered_map<Name*, b2Body*> Physics2D::_bodies;
+std::unordered_map<Name*, b2Fixture*> Physics2D::_fixtures;
 
 void Physics2D::Init()
 {
@@ -10,9 +11,84 @@ void Physics2D::Init()
 
 void Physics2D::Update(float deltaTime)
 {
+	StoryManager::ComputeForEach<
+		Body, 
+		Shape, 
+		Location,
+		Angle>(SetBodyPosition);
+
 	_world->Step(deltaTime, 6, 2);
+
+	StoryManager::ComputeForEach<
+		Body,
+		Shape,
+		Location,
+		Angle>(UpdateFromBodyTransforms);
 }
 
 void Physics2D::LateUpdate(float deltaTime)
 {
+}
+
+Body& Physics2D::CreateBody(Entity& entity)
+{
+	b2BodyDef def;
+	Location location = Location(0.0f, 0.0f);
+	if (entity.Has<Location>())
+		location = entity.Get<Location>();
+	else
+	{
+		entity.Add<Location>(location.X, location.Y);
+	}
+	def.type = b2_dynamicBody;
+	def.position.Set(location.X, location.Y);
+
+	b2Body* b2body = _world->CreateBody(&def);
+	_bodies[&entity.Get<Name>()] = b2body;
+	return entity.Add<Body>(&entity.Get<Name>());
+}
+Shape& Physics2D::CreateBoxShape(Entity& entity)
+{
+	b2PolygonShape shape;
+	Size size = Size(50.0f, 50.0f);
+	if (entity.Has<Size>())
+		size = entity.Get<Size>();
+	else
+	{
+		entity.Add<Size>(size.X, size.Y);
+	}
+	shape.SetAsBox(size.X/2.0f, size.Y/2.0f);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &shape;
+	fixtureDef.density = 1.0f;
+	fixtureDef.friction = 0.3f;
+	auto name = entity.Get<Name>();
+	if (!_bodies.count(&name)) CreateBody(entity);
+	_fixtures[&name] = _bodies[&name]->CreateFixture(&fixtureDef);
+	return entity.Add<Shape>(_fixtures[&name]);
+}
+
+
+void Physics2D::SetBodyPosition(Body body, Shape shape, 
+	Location location, Angle angle)
+{
+	//if (auto name = body.ID.lock())
+	//{
+		auto b2body = _bodies[body.ID];
+
+		b2body->SetTransform(b2Vec2(location.X, location.Y), angle.Value);
+	//}
+}
+void Physics2D::UpdateFromBodyTransforms(Body body, Shape shape,
+	Location location, Angle angle)
+{
+	//if (auto name = body.ID.lock())
+	//{
+		auto b2body = _bodies[body.ID];
+
+		(& location)->X = b2body->GetPosition().x;
+		(& location)->Y = b2body->GetPosition().y;
+		(& angle)->Value = b2body->GetAngle();
+	//}
 }
--- DailyQuestCore/src/Physics/Physics2D.h
+++ DailyQuestCore/src/Physics/Physics2D.h
@@ -1,11 +1,27 @@
 #pragma once
 
 #include <box2d/box2d.h>
+#include <glm/glm.hpp>
 
 #include <memory>
 #include <unordered_map>
 
+#include "Story/Components.h"
+#include "Story/StoryManager.h"
+#include "Story/Entity.h"
+
 #include "Quest/Name.h"
+
+struct Body
+{
+	Name* ID;
+	Body(Name* id) : ID(id) { }
+};
+struct Shape
+{
+	b2Fixture* Fixture;
+	Shape(b2Fixture* fixture) : Fixture(fixture) { }
+};
 
 class Physics2D
 {
@@ -15,16 +31,17 @@ public:
 	static void Update(float deltaTime);
 	static void LateUpdate(float deltaTime);
 
-	/*
-	Maybe make physicsManager create unique_ptr for other components to play
-	*/
-	/*
-	Add physicsobjects here maybe much ?
-	Or tryna make the entity component system now so it doens't get messy early
-	*/
+	static Body& CreateBody(Entity& entity);
+	static Shape& CreateBoxShape(Entity& entity);
 
 private:
 	static std::unique_ptr<b2World> _world;
-	static std::unordered_map<std::shared_ptr<Name>, b2Body*> _bodies;
+	static std::unordered_map<Name*, b2Body*> _bodies;
+	static std::unordered_map<Name*, b2Fixture*> _fixtures;
+
+	static void SetBodyPosition(Body body, Shape shape,
+		Location location, Angle angle);
+	static void UpdateFromBodyTransforms(Body body, Shape shape,
+		Location location, Angle angle);
 };
 
