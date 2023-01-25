f8368a3a5de8852f34688e5c56d84c809aee12c3
Ninjarque <ninjarque@gmail.com>
2023-01-25 12:09:30 +0100

    Déplacement du code de StoryManager.cpp dans son .h

--- DailyQuestCore/src/Story/StoryManager.cpp
+++ DailyQuestCore/src/Story/StoryManager.cpp
@@ -1,33 +1,3 @@
 #include "StoryManager.h"
 
-std::unordered_map<Story*, int> StoryManager::_stories;
-
-void StoryManager::Init()
-{
-}
-
-void StoryManager::Update(float deltaTime)
-{
-	for (auto& story : _stories)
-	{
-		story.first->Update(deltaTime);
-	}
-}
-
-void StoryManager::Draw()
-{
-	for (auto& story : _stories)
-	{
-		story.first->Draw();
-	}
-}
-
-void StoryManager::Add(Story* story)
-{
-	_stories[story] = _stories.size();
-}
-
-void StoryManager::Remove(Story* story)
-{
-	_stories.erase(story);
-}
+std::vector<std::weak_ptr<Story>> StoryManager::_stories;
--- DailyQuestCore/src/Story/StoryManager.h
+++ DailyQuestCore/src/Story/StoryManager.h
@@ -2,25 +2,99 @@
 
 #include <unordered_map>
 #include <vector>
+#include <tuple>
 #include <memory>
 
 #include "Story.h"
 
-class Story;
-
 class StoryManager
 {
 public:
-	static void Init();
-	static void Update(float deltaTime);
-	static void Draw();
+	static void Init()
+	{
+
+	}
+	static void Update(float deltaTime)
+	{
+		for (auto s = _stories.begin(); s != _stories.end(); )
+		{
+			if (auto story = s->lock())
+			{
+				story->Update(deltaTime);
+				++s;
+			}
+			else
+			{
+				//s = std::remove(_stories.begin(), _stories.end(), s);
+				s = _stories.erase(s);
+			}
+		}
+	}
+
+	static void Draw()
+	{
+		for (auto s = _stories.begin(); s != _stories.end(); )
+		{
+			if (auto story = s->lock())
+			{
+				story->Draw();
+				++s;
+			}
+			else
+			{
+				//s = std::remove(_stories.begin(), _stories.end(), s);
+				s = _stories.erase(s);
+			}
+		}
+	}
+
+	static std::shared_ptr<Story> CreateStory()
+	{
+		auto story = std::shared_ptr<Story>(new Story());
+		_stories.push_back(story);
+		return story;
+	}
+
+	template<typename... Component>
+	static std::vector<std::tuple<std::vector<Component>...>> GetInAllStories();
+
+	template<typename... Component>
+	static void ComputeForEach(void (*f)(Component... components));
 
 private:
-	static void Add(Story* story);
-	static void Remove(Story* story);
+	static std::vector<std::weak_ptr<Story>> _stories;
 
-	static std::unordered_map<Story*, int> _stories;
-
-	friend class Story;
+	//friend class Story;
 };
 
+template<typename... Component>
+static std::vector<std::tuple<std::vector<Component>...>> StoryManager::GetInAllStories()
+{
+	std::vector<std::tuple<std::vector<Component>...>> listOfComponents;
+	for (auto s = _stories.begin(); s != _stories.end(); )
+	{
+		if (auto story = s->lock())
+			listOfComponents.push_back(story->GetAll<Component...>());
+		else
+		{
+			//s = std::remove(_stories.begin(), _stories.end(), s);
+			s = _stories.erase(s);
+		}
+	}
+	return listOfComponents;
+}
+
+template<typename... Component>
+static void StoryManager::ComputeForEach(void (*f)(Component... components))
+{
+	auto all = GetInAllStories<Component...>();
+	for (auto& tuple : all)
+	{
+		auto argsTuple = tuple;
+		std::apply([&f](const auto&... argsVectors) {
+			for (int i = 0; i < std::min({ argsVectors.size()... }); ++i) {
+				f(argsVectors[i]...);
+			}
+			}, argsTuple);
+	}
+}
