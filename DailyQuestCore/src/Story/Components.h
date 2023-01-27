abd2d3529fb8af3bb64555b86489b8b184221c86
Ninjarque <ninjarque@gmail.com>
2023-01-27 01:23:04 +0100

    Implémentation d'un sytème de lien automatique de component avec son Entitée parente quand créé

--- DailyQuestCore/src/Story/Components.h
+++ DailyQuestCore/src/Story/Components.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Entity.h"
+
 struct Location
 {
 	float X;
@@ -20,3 +22,90 @@ struct Angle
 	Angle() : Value(0.0f) { }
 	Angle(float value) : Value(value) { }
 };
+
+struct Visibility
+{
+	bool Visible;
+	Visibility() : Visible(true) { }
+	Visibility(bool visible) : Visible(visible) { }
+};
+struct Depth
+{
+	float Value;
+	Depth() : Value(0.0f) { }
+	Depth(float value) : Value(value) { }
+};
+// /*
+class Childrens
+{
+public:
+	template<typename... Args>
+	Childrens(Entity current, Args... childs) : _current(current) 
+	{
+		std::vector<Entity> childEntities = { childs... };
+		for (int i = 0; i < childEntities.size(); i++)
+			Add(childEntities[i]);
+	}
+	~Childrens() { _childs.clear(); }
+	void Add(Entity child) 
+	{ 
+		if (!child.Has<Parent>())
+		{
+			child.Add<Parent>( _current);
+		}
+		else
+			child.Set<Parent>( _current);
+		_childs.push_back(child); 
+	}
+	bool Remove(Entity child) 
+	{
+		for (auto it = _childs.begin(); it != _childs.end(); )
+		{
+			auto entity = *it;
+			if (!entity || entity == child)
+			{
+				it = _childs.erase(it);
+			}
+			else
+				++it;
+		}
+	}
+	std::vector<Entity> GetChildrens()
+	{
+		std::vector<Entity> res;
+		for (auto it = _childs.begin(); it != _childs.end(); )
+		{
+			auto entity = *it;
+			if (!entity)
+			{
+				it = _childs.erase(it);
+			}
+			else
+			{
+				res.push_back(entity);
+				++it;
+			}
+		}
+		return res;
+	}
+
+private:
+	Entity _current;
+	std::vector<Entity> _childs;
+	friend class Parent;
+};
+class Parent
+{
+public:
+	Parent(Entity current, Entity parent) 
+		: _current(current), _parent(parent) { }
+	~Parent() { }
+
+	Entity GetParent() { return _parent; }
+private:
+	Entity _current;
+	Entity _parent;
+
+	friend class Childrens;
+};
+// */
--- DailyQuestCore/src/Story/Entity.h
+++ DailyQuestCore/src/Story/Entity.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <entt/entt.hpp>
+#include <tuple>
+#include <type_traits>
+#include <iostream>
 
 #include "Error.h"
 
@@ -45,6 +48,22 @@ public:
 	}
 
 	operator bool() const { return _registry != nullptr && !_storyInformations.expired(); }
+
+	bool operator==(const Entity& other) const
+	{
+		if (other)
+		{
+			if (auto storyInformations = _storyInformations.lock())
+				if (auto otherInformations = other._storyInformations.lock())
+				{
+					auto thisHandle = _handle.get();
+					auto otherHandle = other._handle.get();
+					return storyInformations.get() == otherInformations.get() && thisHandle == otherHandle;
+				}
+		}
+		return false;
+	}
+	bool operator!=(const Entity& other) const { return !(*this == other); }
 private:
 	Entity(std::weak_ptr<StoryInformations> storyInformations, std::shared_ptr<entt::registry> registry)
 	{
@@ -85,19 +104,62 @@ T& Entity::Get()
 	return _registry->get<T>(*(_handle.get()));
 }
 
+template<typename T, typename... Args>
+//typename std::enable_if_t<!std::is_base_of<EntityLinkedComponent, T>::value, T&>
+typename std::enable_if_t<!std::is_constructible<T, Entity, Args...>::value, T&>
+emplace(entt::registry* registry, std::shared_ptr<entt::entity> handle, Entity* entity, Args&&... args) {
+	return registry->emplace<T>(*(handle.get()), std::forward<Args>(args)...);
+}
+template<typename T, typename... Args>
+//typename std::enable_if_t<std::is_base_of<EntityLinkedComponent, T>::value, T&>
+typename std::enable_if_t<std::is_constructible<T, Entity, Args...>::value, T&>
+emplace(entt::registry* registry, std::shared_ptr<entt::entity> handle, Entity* entity, Args&&... args) {
+	std::cout << "Add from a linked component!" << std::endl;
+	return registry->emplace<T>(*(handle.get()), *entity, std::forward<Args>(args)...);
+}
+
+template<typename T, typename... Args>
+//typename std::enable_if_t<!std::is_base_of<EntityLinkedComponent, T>::value, T&>
+typename std::enable_if_t<!std::is_constructible<T, Entity, Args...>::value, T&>
+emplace_or_replace(entt::registry* registry, std::shared_ptr<entt::entity> handle, Entity* entity, Args&&... args) {
+	return registry->emplace_or_replace<T>(*(handle.get()), std::forward<Args>(args)...);
+}
+template<typename T, typename... Args>
+//typename std::enable_if_t<std::is_base_of<EntityLinkedComponent, T>::value, T&>
+typename std::enable_if_t<std::is_constructible<T, Entity, Args...>::value, T&>
+emplace_or_replace(entt::registry* registry, std::shared_ptr<entt::entity> handle, Entity* entity, Args&&... args) {
+	std::cout << "Set from a linked component!" << std::endl;
+	return registry->emplace_or_replace<T>(*(handle.get()), *entity, std::forward<Args>(args)...);
+}
 
 template<typename T, typename ...Args>
 T& Entity::Add(Args&&... args)
 {
 	if (Has<T>()) Error::fatalError("Could not add component because it already has been added!");
-
+	/*
+	if constexpr (std::is_same_v<T, EntityLinkedComponent>)
+	{
+		//ComponentData<T, Args...> data{ *this, std::forward_as_tuple(std::forward<Args>(args)...) };
+		//return _registry->emplace<T>(*(_handle.get()), data);
+		return _registry->emplace<T>(*(_handle.get()), *this, std::forward<Args>(args)...);
+	}
 	return _registry->emplace<T>(*(_handle.get()), std::forward<Args>(args)...);
+	*/
+	return emplace<T>(_registry, _handle, this, std::forward<Args>(args)...);
 }
 
 template<typename T, typename ...Args>
 T& Entity::Set(Args&&... args)
 {
+	/*
+	if constexpr (std::is_same_v<T, EntityLinkedComponent>)
+	{
+		return _registry->emplace_or_replace<T>(*(_handle.get()), *this, std::forward<Args>(args)...);
+	}
+	
 	return _registry->emplace_or_replace<T>(*(_handle.get()), std::forward<Args>(args)...);
+	*/
+	return emplace_or_replace<T>(_registry, _handle, this, std::forward<Args>(args)...);
 }
 
 template<typename T>
